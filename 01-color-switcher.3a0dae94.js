71c944cade7e4adcd3078fafb2db6f92d2eb0764
NatalieFalach <natkacandy@gmail.com>
2022-11-09 11:38:08 +0000

    Deploying to gh-pages from @ NatalieFalach/goit-js-hw-09@297f997885749fdfcfb7769b413d277e10d3b195 🚀

---
+++ 01-color-switcher.3a0dae94.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(d){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1,e.disabled=!0}));var a=null;e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.3a0dae94.js.map
